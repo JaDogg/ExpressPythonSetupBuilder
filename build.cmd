912c242a7dbf0bbdcbc31314590367f9c98de26c
JaDogg <jadogg.coder@gmail.com>
2014-12-13 21:44:44 +0530

    Build Setp exe

--- build.cmd
+++ build.cmd
@@ -13,8 +13,11 @@ CD %BUILD_PATH%
 SET Z_PATH=%BUILD_PATH%7z\
 SET WORKAT=%BUILD_PATH%ExpressPython\
 SET PACKSAT=%BUILD_PATH%packs\
+SET OUTPUTAT=%BUILD_PATH%output\
+SET EXPRESS_7Z_OUT=ExpressPython.7z
+SET EXPRESS_EXE_OUT=ExpressPythonSetup.exe
 MKDIR %BUILD_PATH%ExpressPython > NUL 2>&1
-MKDIR %BUILD_PATH%compilers > NUL 2>&1
+MKDIR %BUILD_PATH%output > NUL 2>&1
 REM
 REM
 REM Full path to compiled Express Python, Modify this accordingly
@@ -24,9 +27,11 @@ REM
 REM Extract compilers if we don't have them
 REM
 IF EXIST %BUILD_PATH%compilers GOTO COMPILERS_AVAILABLE
+MKDIR %BUILD_PATH%compilers > NUL 2>&1
 7z x -y -o%BUILD_PATH%compilers %PACKSAT%FreeBASIC-1.00.0-win32.7z
+7z x -y -o%BUILD_PATH%compilers %PACKSAT%NSIS.7z
 :COMPILERS_AVAILABLE
-SET PATH=%PATH%;%BUILD_PATH%compilers\FreeBASIC-1.00.0-win32\
+SET PATH=%PATH%;%BUILD_PATH%compilers\FreeBASIC-1.00.0-win32\;%BUILD_PATH%compilers\NSIS\
 REM
 REM Create Launcher
 REM
@@ -48,15 +53,17 @@ REM
 COPY /Y %PACKSAT%snippets.dat %WORKAT% > NUL 2>&1
 ECHO Extracting Python...
 IF EXIST %WORKAT%Python34 GOTO PYTHON_EXTRACTED
-7z x -y -o%WORKAT% %PACKSAT%Python34.7z > NUL 2>&1
+7z x -y -o%WORKAT% %PACKSAT%Python34.7z
 :PYTHON_EXTRACTED
 ECHO Extracting Completed
-ECHO Running..
-START %WORKAT%launch.exe
-PAUSE
-ECHO Creating ExpressPython.7z...
+ECHO Creating %EXPRESS_7Z_OUT% ...
 7z a -y ExpressPython.7z ExpressPython\ -mx9
 CLS
 ECHO Success
-
-
+MOVE /Y %EXPRESS_7Z_OUT% %OUTPUTAT% > NUL 2>&1
+ECHO Creating ExpressPythonSetup.exe ...
+makensisw Setup.nsi
+ECHO Success
+MOVE /Y %EXPRESS_EXE_OUT% %OUTPUTAT% > NUL 2>&1
+START %OUTPUTAT%
+PAUSE
